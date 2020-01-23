70f41735550e1668b084008976914de870b817f0
Heike Miersch <heike.miersch@hotmail.de>
2020-01-23 14:37:38 +0100

    login page

---
+++ src/main/resources/static/web/login.js
@@ -0,0 +1,23 @@
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('gp');
+
+var page = new Vue({
+    el: "#page",
+    data: {
+
+
+    },
+    methods: {
+
+        login() {
+
+        },
+
+        logout() {
+
+        }
+
+
+    }
+
+})
